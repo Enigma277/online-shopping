eb9b12b7465b5f78dd58d2a6f4515499e73a7bd1
enigma277 <dgour313@gmail.com>
2018-09-29 02:16:01 +0530

    satyajeet ne kar diya

--- onlineshopping/src/main/java/enigma27/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/enigma27/onlineshopping/controller/PageController.java
@@ -2,43 +2,115 @@ package enigma27.onlineshopping.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import enigma27.shoppingbackend.dao.CategoryDAO;
+import enigma27.shoppingbackend.dto.Category;
 
 @Controller
-public class PageController {
-	
+public class PageController 
+{
 	@Autowired
 	private CategoryDAO categoryDAO;
 	
-	@RequestMapping(value = {"/","/home","/index"})
-	public ModelAndView index() {
+	
+	@RequestMapping(value= {"/","/home","/index"})
+	public ModelAndView index() 
+	{
 		
-		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("title" ,"Home");
+		ModelAndView mv= new ModelAndView("page"); 
+		mv.addObject("title","Home");
 		
-		// passing list of category
+		//passing the list of categories
 		mv.addObject("categories",categoryDAO.list());
-		mv.addObject("userClickHome", true);
+		mv.addObject("userClickHome",true);	
 		return mv;
 	}
 	
-	@RequestMapping(value = "/about")
-	public ModelAndView about() {
+	@RequestMapping(value="/about")
+	public ModelAndView about() 
+	{
 		
-		ModelAndView mv = new ModelAndView("page");
+		ModelAndView mv= new ModelAndView("page"); 
 		mv.addObject("title","About Us");
-		mv.addObject("userClickAbout", true);
+		mv.addObject("userClickAbout",true);	
 		return mv;
 	}
-	@RequestMapping(value = "/contact")
-	public ModelAndView contact() {
+	
+	@RequestMapping(value="/contact")
+	public ModelAndView contact()
+	{
 		
-		ModelAndView mv = new ModelAndView("page");
+		ModelAndView mv= new ModelAndView("page");
+		//System.out.println("Hello");
 		mv.addObject("title","Contact Us");
-		mv.addObject("userClickContact", true);
+		mv.addObject("userClickContact",true);	
+		return mv;//
+	}
+	
+	/*
+	 * Methods to load all the products based on category
+	 */
+	
+	@RequestMapping(value="/show/all/products")
+	public ModelAndView showAllProducts() 
+	{
+		
+		ModelAndView mv= new ModelAndView("page"); 
+		mv.addObject("title","All Products");
+		//System.out.println("Hello");
+		//passing the list of categories
+		mv.addObject("categories",categoryDAO.list());
+		mv.addObject("userClickAllProducts",true);	
 		return mv;
 	}
+	
+	@RequestMapping(value="/show/category/{id}/products")
+	public ModelAndView showCategoryProducts(@PathVariable("id") int id) 
+	{
+		
+		ModelAndView mv= new ModelAndView("page");
+		
+		//categoryDAO to fetch a single category
+		Category category=null;
+		category=categoryDAO.get(id);
+		
+		mv.addObject("title",category.getName());
+		
+		//passing the list of categories
+		mv.addObject("categories",categoryDAO.list());
+		mv.addObject("category",category); 
+		mv.addObject("userClickCategoryProducts",true);	
+		return mv;
+	}
+	
+	
+/*	@RequestMapping(value="/test")
+	public ModelAndView test(@RequestParam(value="greeting",required=false)String greeting) 
+	{
+		if(greeting == null) 
+		{
+			greeting="Hi There";
+		}
+			
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return(mv);  //
+	}*/
+	
+/*	//Path Variable Not Working
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting")String greeting) {
+		if(greeting == null) {
+			greeting="Hey There";
+		}
+		
+		ModelAndView mv= new ModelAndView("page"); //
+		mv.addObject("greeting",greeting); //
+		return(mv); //
+	}*/
+	
+
 }
